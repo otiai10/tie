d40beb54164dbdcc953c20a640403498a2e54391
Hiromu OCHIAI <otiai10@gmail.com>
2019-10-15 09:57:18 -0300

    Make the test more stable

--- too/all_test.go
+++ too/all_test.go
@@ -18,5 +18,6 @@ func Test_MainAction(t *testing.T) {
 	ctx := cli.NewContext(app, set, nil)
 	err := MainAction(ctx)
 	Expect(t, err).ToBe(nil)
-	Expect(t, buf.String()).ToBe("[0] echo\tfoo\n[1] echo\tbaa\n")
+	Expect(t, buf.String()).Match("\\[0\\] echo\tfoo\n")
+	Expect(t, buf.String()).Match("\\[1\\] echo\tbaa\n")
 }
